9074a2266812a40c78c31ced36eae74e11ea7cb7
Ran_Zilberman <ranz@wix.com>
2016-03-21 11:36:34 +0200

    proper output display

--- index.js
+++ index.js
@@ -7,8 +7,8 @@ var path = require('path');
 var fullPath = path.join(__dirname, 'code-sample.ts');
 var code = fs.readFileSync(fullPath, 'utf8');
 
-var result = JSON.stringify(typescriptEslintParser.parse(code, {}));
-console.log(result);
+var result = JSON.stringify(typescriptEslintParser.parse(code, {}), null, 4);
+console.log('typescript-eslint-parser AST result: ' + '\n' + result + '\n');
 
 var opts = {
   "outputFile": false,
@@ -17,6 +17,7 @@ var opts = {
   "configFile": ".eslintrc"
 };
 var engine = new eslint.CLIEngine(opts);
+console.log('Running eslint on AST result:\n');
 
 var report;
 try {
